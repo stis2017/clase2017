07f65e93c669e58c5b6d7cf033fc6ad3af056e1a
pc <pc@172.16.17.122>
2017-03-30 16:53:35 -0600

    Iniciacion de el primer proyecto en Git

---
+++ src/clase2017/Clase2017.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clase2017;
+
+/**
+ *
+ * @author pc
+ */
+public class Clase2017 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
